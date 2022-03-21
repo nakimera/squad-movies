4e59e9322c8b836ab11b86a5a109d22f57717627
nakimera <prossienakimera@gmail.com>
2022-03-21 19:13:10 +0300

    fetch movies

--- src/app.test.js
+++ src/app.test.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
+import { fetchMovies } from "./fetcher";
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('throws error for unauthourized users', () => {
+  // expect(() => fetchMovies()))
+})
+
+it('returns movies data', () => {
+
 });
